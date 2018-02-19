898620c35292aba3808eb391d9f8a04c8217c75d
3milychu <3milychu@gmail.com>
2018-02-18 21:06:30 -0500

    version 2

--- sketch.js
+++ sketch.js
@@ -14,6 +14,7 @@ var glassData;
 var steelData;
 var silverData;
 var goldData;
+var woodSilkData;
 
 var objectNames;
 var repImg1;
@@ -85,8 +86,6 @@ d3.csv("https://media.githubusercontent.com/media/3milychu/majorstudio/master/la
 				  	.entries(woodData)
 				  	.sort(function(a,b) {return d3.ascending(a.key,b.key);});
 
-			    // console.log(woodDataUse);
-
 			   // Data for "silk" selection
 			   silkData = data.filter(function(d) { 
 			    	return d.hasSilk == 1
@@ -98,7 +97,6 @@ d3.csv("https://media.githubusercontent.com/media/3milychu/majorstudio/master/la
 				  	.entries(silkData)
 				  	.sort(function(a,b) {return d3.ascending(a.key,b.key);});
 
-			    // console.log(silkDataUse);
 
 			    // Data for "ink" selection
 			   inkData = data.filter(function(d) { 
@@ -111,8 +109,6 @@ d3.csv("https://media.githubusercontent.com/media/3milychu/majorstudio/master/la
 				  	.entries(inkData)
 				  	.sort(function(a,b) {return d3.ascending(a.key,b.key);});
 
-			    // console.log(inkDataUse);
-
 			     // Data for "silver" selection
 			   silverData = data.filter(function(d) { 
 			    	return d.hasSilver== 1
@@ -123,8 +119,6 @@ d3.csv("https://media.githubusercontent.com/media/3milychu/majorstudio/master/la
 				  	.rollup(function(v) { return v.length; })
 				  	.entries(silverData)
 				  	.sort(function(a,b) {return d3.ascending(a.key,b.key);});
-
-			    // console.log(inkDataUse);
 
 
 			     // Data for "glass" selection
@@ -138,8 +132,6 @@ d3.csv("https://media.githubusercontent.com/media/3milychu/majorstudio/master/la
 				  	.entries(glassData)
 				  	.sort(function(a,b) {return d3.ascending(a.key,b.key);});
 
-			    // console.log(inkDataUse);
-
 			     // Data for "steel" selection
 			   steelData = data.filter(function(d) { 
 			    	return d.hasSteel == 1
@@ -151,7 +143,6 @@ d3.csv("https://media.githubusercontent.com/media/3milychu/majorstudio/master/la
 				  	.entries(steelData)
 				  	.sort(function(a,b) {return d3.ascending(a.key,b.key);});
 
-			    // console.log(inkDataUse);
 
 			     // Data for "gold" selection
 			   goldData = data.filter(function(d) { 
@@ -164,13 +155,181 @@ d3.csv("https://media.githubusercontent.com/media/3milychu/majorstudio/master/la
 				  	.entries(goldData)
 				  	.sort(function(a,b) {return d3.ascending(a.key,b.key);});
 
-			    // console.log(inkDataUse);
+  				// Data for "wood and silk" selection
+			   woodSilkData = data.filter(function(d) { 
+			    	return d.hasWood == 1 & d.hasSilk == 1
+			    	});
 
+			   woodSilkDataUse = d3.nest()
+			   		.key(function(d) { return d.objectBeginDate; })
+				  	.rollup(function(v) { return v.length; })
+				  	.entries(woodSilkData)
+				  	.sort(function(a,b) {return d3.ascending(a.key,b.key);});
+
+				// Data for "wood and ink" selection
+			   woodInkData = data.filter(function(d) { 
+			    	return d.hasWood == 1 & d.hasInk == 1
+			    	});
+
+			   woodInkDataUse = d3.nest()
+			   		.key(function(d) { return d.objectBeginDate; })
+				  	.rollup(function(v) { return v.length; })
+				  	.entries(woodInkData)
+				  	.sort(function(a,b) {return d3.ascending(a.key,b.key);});
+
+				// Data for "wood and silver" selection
+			   woodSilverData = data.filter(function(d) { 
+			    	return d.hasWood == 1 & d.hasSilver == 1
+			    	});
+
+			   woodSilverDataUse = d3.nest()
+			   		.key(function(d) { return d.objectBeginDate; })
+				  	.rollup(function(v) { return v.length; })
+				  	.entries(woodSilverData)
+				  	.sort(function(a,b) {return d3.ascending(a.key,b.key);});
 				change(allData);
+
+
+				// Data for "wood and glass" selection
+			   woodGlassData = data.filter(function(d) { 
+			    	return d.hasWood == 1 & d.hasGlass == 1
+			    	});
+
+			   woodGlassDataUse = d3.nest()
+			   		.key(function(d) { return d.objectBeginDate; })
+				  	.rollup(function(v) { return v.length; })
+				  	.entries(woodGlassData)
+				  	.sort(function(a,b) {return d3.ascending(a.key,b.key);});
+
+				  	// Data for "wood and steel" selection
+			   woodSteelData = data.filter(function(d) { 
+			    	return d.hasWood == 1 & d.hasSteel == 1
+			    	});
+
+			   woodSteelDataUse = d3.nest()
+			   		.key(function(d) { return d.objectBeginDate; })
+				  	.rollup(function(v) { return v.length; })
+				  	.entries(woodSteelData)
+				  	.sort(function(a,b) {return d3.ascending(a.key,b.key);});
+
+				  	// Data for "wood and gold" selection
+			   woodGoldData = data.filter(function(d) { 
+			    	return d.hasWood == 1 & d.hasGold == 1
+			    	});
+
+			   woodGoldDataUse = d3.nest()
+			   		.key(function(d) { return d.objectBeginDate; })
+				  	.rollup(function(v) { return v.length; })
+				  	.entries(woodGoldData)
+				  	.sort(function(a,b) {return d3.ascending(a.key,b.key);});
 
 // define change datasetTotal
 function change(dataset) {
 
+	var name;
+	var totalRows = dataset.length;
+
+	if (dataset == woodData){
+		d3.select(".cover").selectAll("img").remove();
+
+		var displayMet= d3.select(".cover").selectAll("#cover1")
+			.data(dataset.filter(function (d, i) { return i === 0;}))
+	        .enter()
+	        .append('img')
+	        .style("width","100%")
+	        .style("height","100%")
+	        .attr("src","assets/wood.jpg")
+	       	.style("background-position","center center")
+	        .style("background-size","30%")
+	        .style("position","relative")
+	        .exit();
+	} else if (dataset == silkData) {
+		d3.select(".cover").selectAll("img").remove();
+
+		var displayMet= d3.select(".cover").selectAll("#cover1")
+			.data(dataset.filter(function (d, i) { return i === 0;}))
+	        .enter()
+	        .append('img')
+	        .style("width","100%")
+	        .style("height","100%")
+	        .attr("src","assets/silk.jpg")
+	       	.style("background-position","center center")
+	        .style("background-size","30%")
+	        .style("position","relative")
+	        .exit();
+	} else if (dataset == inkData) {
+		d3.select(".cover").selectAll("img").remove();
+
+		var displayMet= d3.select(".cover").selectAll("#cover1")
+			.data(dataset.filter(function (d, i) { return i === 0;}))
+	        .enter()
+	        .append('img')
+	        .style("width","100%")
+	        .style("height","100%")
+	        .attr("src","assets/ink.jpg")
+	       	.style("background-position","center center")
+	        .style("background-size","30%")
+	        .style("position","relative")
+	        .exit();
+	} else if (dataset == silverData) {
+		d3.select(".cover").selectAll("img").remove();
+
+		var displayMet= d3.select(".cover").selectAll("#cover1")
+			.data(dataset.filter(function (d, i) { return i === 0;}))
+	        .enter()
+	        .append('img')
+	        .style("height","100%")
+	        .style("overflow-y","hidden")
+	        .attr("src","assets/silver.jpg")
+	       	.style("background-position","center center")
+	        .style("background-size","30%")
+	        .style("position","relative")
+	        .exit();
+	} else if (dataset == glassData) {
+		d3.select(".cover").selectAll("img").remove();
+
+		var displayMet= d3.select(".cover").selectAll("#cover1")
+			.data(dataset.filter(function (d, i) { return i === 0;}))
+	        .enter()
+	        .append('img')
+	        .style("width","100%")
+	        .style("height","100%")
+	        .attr("src","assets/glass.jpg")
+	       	.style("background-position","center center")
+	        .style("background-size","30%")
+	        .style("position","relative")
+	        .exit();
+	} else if (dataset == steelData) {
+		d3.select(".cover").selectAll("img").remove();
+
+		var displayMet= d3.select(".cover").selectAll("#cover1")
+			.data(dataset.filter(function (d, i) { return i === 0;}))
+	        .enter()
+	        .append('img')
+	        .style("width","100%")
+	        .style("height","100%")
+	        .attr("src","assets/steel.jpg")
+	       	.style("background-position","center center")
+	        .style("background-size","30%")
+	        .style("position","relative")
+	        .exit();
+	} else if (dataset == goldData) {
+		d3.select(".cover").selectAll("img").remove();
+
+		var displayMet= d3.select(".cover").selectAll("#cover1")
+			.data(dataset.filter(function (d, i) { return i === 0;}))
+	        .enter()
+	        .append('img')
+	        .style("width","100%")
+	        .style("height","100%")
+	        .attr("src","assets/gold.jpg")
+	       	.style("background-position","center center")
+	        .style("background-size","30%")
+	        .style("position","relative")
+	        .exit();
+	};
+	
+	window.location.href = "#top";
 // end change dataset function
 };
 
@@ -181,6 +340,7 @@ function origins(dataset) {
 	// console.log(totalRows);
 
 	var format = d3.format(".0%");
+	
 
 	var origins = d3.nest()
    		.key(function(d) { return d.Culture; })
@@ -246,6 +406,58 @@ function origins(dataset) {
 	        .attr("id", "culture7")
 	        .text(function(d) { return d.key + " " + format(d.value/totalRows); })
 	        .exit();
+
+
+	// Origins Gauge
+
+	d3.select(".gauge").selectAll("div").remove();
+	var gauge1 = d3.select(".gauge").selectAll("#gaugeFill")
+			.data(origins.filter(function (d, i) { return i === 0;}))
+	        .enter()
+	        .append("div")
+	        .attr("id", "gauge1")
+	        .style("width",function(d) {return format(d.value/totalRows); })
+	        .exit();
+	var div1 = d3.select(".gauge").selectAll("#gaugeFill")
+			.data(origins.filter(function (d, i) { return i === 0;}))
+	        .enter()
+	        .append("div")
+	        .style("width",function(d) {return format(d.value/totalRows); })
+	        .exit();
+	var div2 = d3.select(".gauge").selectAll("#gaugeFill")
+			.data(origins.filter(function (d, i) { return i === 0;}))
+	        .enter()
+	        .append("div")
+	        .style("width",function(d) {return format(d.value/totalRows); })
+	        .exit();
+
+	var gauge1 = d3.select(".gauge").selectAll("#gaugeFill")
+			.data(origins.filter(function (d, i) { return i === 1;}))
+	        .enter()
+	        .append("div")
+	        .attr("id", "gauge2")
+	        .style("width",function(d) {return format(d.value/totalRows); })
+	        .exit();
+	var div1 = d3.select(".gauge").selectAll("#gaugeFill")
+			.data(origins.filter(function (d, i) { return i === 1;}))
+	        .enter()
+	        .append("div")
+	        .style("width",function(d) {return format(d.value/totalRows); })
+	        .exit();
+	var div2 = d3.select(".gauge").selectAll("#gaugeFill")
+			.data(origins.filter(function (d, i) { return i === 1;}))
+	        .enter()
+	        .append("div")
+	        .style("width",function(d) {return format(d.value/totalRows); })
+	        .exit();
+
+	var gauge1 = d3.select(".gauge").selectAll("#gaugeFill")
+			.data(origins.filter(function (d, i) { return i === 2;}))
+	        .enter()
+	        .append("div")
+	        .attr("id", "gauge3")
+	        .style("width",function(d) {return format(d.value/totalRows); })
+	        .exit();
     // end update origins function
     };
 
@@ -276,6 +488,7 @@ function origins(dataset) {
 	};
 
 	var format = d3.format(".0%");
+	var formatThousands = d3.format(",");
 
 	objectNames = d3.nest()
 		.key(function(d) { return d.objectName; })
@@ -322,7 +535,7 @@ function origins(dataset) {
 	        .enter()
 	        .append("text")
 	        .attr("id", "objectTitle")
-	        .text(function(d) { return d.Title.substring(0,60) })
+	        .text(function(d) { return d.Title.substring(0,40) })
 	        .exit();
 
 	var displayDate = d3.select(".overlay").selectAll("#overlay1")
@@ -354,7 +567,17 @@ function origins(dataset) {
 	        .enter()
 	        .append("text")
 	        .attr("id", "objectName")
-	        .text(function(d) {return "Family: " + d.objectName +"s (of " + objectNames[0].value + ")"})
+	        .text(function(d) {return "Family: " + d.objectName +"s (of " + formatThousands(objectNames[0].value) + ")"})
+	        .exit();
+
+	var displayLink= d3.select(".overlay").selectAll("#overlay1")
+	        .data(repImg1.filter(function (d, i) { return i === 0;}))
+	        .enter()
+	        .append("a")
+	        .attr("id", "link")
+	        .attr("href",function(d) {return d.linkResolution})
+	        .attr("target","_blank")
+	        .text("View")
 	        .exit();
 
 	// Rep Image 2
@@ -387,7 +610,7 @@ function origins(dataset) {
 	        .enter()
 	        .append("text")
 	        .attr("id", "objectTitle")
-	        .text(function(d) { return d.Title.substring(0,60)})
+	        .text(function(d) { return d.Title.substring(0,40)})
 	        .exit();
 
 	var displayDate2 = d3.select(".overlay2").selectAll("#overlay2")
@@ -419,7 +642,17 @@ function origins(dataset) {
 	        .enter()
 	        .append("text")
 	        .attr("id", "objectName")
-	        .text(function(d) {return "Family: " + d.objectName +"s (of " + objectNames[1].value + ")"})
+	        .text(function(d) {return "Family: " + d.objectName +"s (of " + formatThousands(objectNames[1].value) + ")"})
+	        .exit();
+
+	var displayLink2= d3.select(".overlay2").selectAll("#overlay2")
+	        .data(repImg2.filter(function (d, i) { return i === 0;}))
+	        .enter()
+	        .append("a")
+	        .attr("id", "link")
+	        .attr("href",function(d) {return d.linkResolution})
+	        .attr("target","_blank")
+	        .text("View")
 	        .exit();
 
 	// Rep Image 3
@@ -453,7 +686,7 @@ function origins(dataset) {
 	        .enter()
 	        .append("text")
 	        .attr("id", "objectTitle")
-	        .text(function(d) { return d.Title.substring(0,60) })
+	        .text(function(d) { return d.Title.substring(0,40) })
 	        .exit();
 
 	var displayDate3 = d3.select(".overlay3").selectAll("#overlay3")
@@ -485,7 +718,30 @@ function origins(dataset) {
 	        .enter()
 	        .append("text")
 	        .attr("id", "objectName")
-	        .text(function(d) {return "Family: " + d.objectName +"s (of " + objectNames[2].value + ")"})
+	        .text(function(d) {return "Family: " + d.objectName +"s (of " + formatThousands(objectNames[2].value) + ")"})
+	        .exit();
+
+	var displayLink= d3.select(".overlay3").selectAll("#overlay3")
+	        .data(repImg3.filter(function (d, i) { return i === 0;}))
+	        .enter()
+	        .append("a")
+	        .attr("id", "link")
+	        .attr("href",function(d) {return d.linkResolution})
+	        .attr("target","_blank")
+	        .text("View")
+	        .exit();
+
+	 // Total Count
+
+	d3.select(".container-micro").selectAll("text").remove();
+
+	var displayTotal = d3.select(".container-micro").selectAll("#total")
+			.data(repImg3.filter(function (d, i) { return i === 0;}))
+	        .enter()
+	        .append("text")
+	        .attr("class", "section-header")
+	        .attr("id", "summary")
+	        .text(formatThousands(totalRows) + " Items")
 	        .exit();
 
 	// Department Filter
@@ -496,6 +752,57 @@ function origins(dataset) {
 	  	.entries(dataset)
 	  	.sort(function(a,b) {return d3.descending(a.value,b.value);});
 	console.log(departments);
+
+	var top1 = dataset.filter(function(d){return d.isPublic === "TRUE" & d.URL != "NA" & d.Department == departments[0].key });
+	// console.log(repImg1);
+	var top2 = dataset.filter(function(d){return d.isPublic === "TRUE" & d.URL != "NA" & d.Department== departments[1].key });
+	// console.log(repImg2);
+	var top3 = dataset.filter(function(d){return d.isPublic === "TRUE" & d.URL != "NA" & d.Department == departments[2].key });
+	// console.log(repImg3);
+
+	d3.select(".departments1").selectAll("img").remove();
+
+	var displayTop1= d3.select(".departments1").selectAll("#top1")
+			.data(top1.filter(function (d, i) { return i === 0;}))
+	        .enter()
+	        .append('img')
+	        .style("width","100%")
+	        .style("overflow-y","hidden")
+	        .attr("src",function(d) {return d.URL;})
+	       	.style("background-position","center center")
+	        .style("background-size","30%")
+	        .style("position","relative")
+	        .exit();
+
+	d3.select(".departments2").selectAll("img").remove();
+
+	var displayTop1= d3.select(".departments2").selectAll("#top2")
+			.data(top2.filter(function (d, i) { return i === 0;}))
+	        .enter()
+	        .append('img')
+	        .style("width","100%")
+	        .style("overflow-y","hidden")
+	        .attr("src",function(d) {return d.URL;})
+	       	.style("background-position","center center")
+	        .style("background-size","30%")
+	        .style("position","relative")
+	        .exit();
+
+	d3.select(".departments3").selectAll("img").remove();
+
+	var displayTop1= d3.select(".departments3").selectAll("#top3")
+			.data(top3.filter(function (d, i) { return i === 0;}))
+	        .enter()
+	        .append('img')
+	        .style("width","100%")
+	        .style("overflow-y","hidden")
+	        .attr("src",function(d) {return d.URL;})
+	       	.style("background-position","center center")
+	        .style("background-size","30%")
+	        .style("position","relative")
+	        .exit();
+
+	d3.select(".info1").selectAll("text").remove();
 
 	
 	d3.select(".medium").selectAll("text").remove();
@@ -590,46 +897,53 @@ function origins(dataset) {
 			        }
 			        else if (value == "Wood")
 			        {
-			            change(woodDataUse);
+			            change(woodData);
 			            origins(woodData);
 			            gallery(woodData);
 			        }
+			        else if (value == "Wood" & value == "Silk")
+			        {
+			            change(woodSilkData);
+			            origins(woodSilkData);
+			            gallery(woodSilkData);
+			        }
 			        else if (value == "Silk")
 			        {
-			            change(silkDataUse);
+			            change(silkData);
 			            origins(silkData);
 			            gallery(silkData);
 			        }
 			        else if (value == "Ink")
 			        {
-			            change(inkDataUse);
+			            change(inkData);
 			            origins(inkData);
 			            gallery(inkData);
 			        }
 			        else if (value == "Silver")
 			        {
-			            change(silverDataUse);
+			            change(silverData);
 			            origins(silverData);
 			            gallery(silverData);
 			        }
 			        else if (value == "Glass")
 			        {
-			            change(glassDataUse);
+			            change(glassData);
 			            origins(glassData);
 			            gallery(glassData);
 			        }
 			        else if (value == "Steel")
 			        {
-			            change(steelDataUse);
+			            change(steelData);
 			            origins(steelData);
 			            gallery(steelData);
 			        }
 			        else if (value == "Gold")
 			        {
-			            change(goldDataUse);
+			            change(goldData);
 			            origins(goldData);
 			            gallery(goldData);
 			        }
+			        
 			    }
 
 //end d3.csv function
