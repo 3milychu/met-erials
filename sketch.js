bb3477b829f951f652160cb0f94627b08086c722
3milychu <3milychu@gmail.com>
2018-02-20 14:38:28 -0500

    push

--- sketch.js
+++ sketch.js
@@ -25,6 +25,10 @@ var repImg3;
 
 function setup(){
 
+	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+		document.getElementById("mobile").style.display="inline";
+	};
+
 	noCanvas();
 
 	getInfo();
